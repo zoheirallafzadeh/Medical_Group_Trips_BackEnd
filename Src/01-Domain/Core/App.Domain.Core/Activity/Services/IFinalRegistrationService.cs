955b7edf1889507451de8e14169fa8395aba08bb
Zoheir Allafzadeh <116369321+zoheirallafzadeh@users.noreply.github.com>
2024-05-22 09:02:32 +0330

    IFinalRegistrationService

---
+++ Src/01-Domain/Core/App.Domain.Core/Activity/Services/IFinalRegistrationService.cs
@@ -0,0 +1,12 @@
+﻿using App.Domain.Core.Activity.DTOs;
+
+namespace App.Domain.Core.Activity.Services
+{
+    public interface IFinalRegistrationService
+    {
+        public Task Add(int ActivityId, int PersoneId, int ResponsibilityId, int PersonCreatorId, CancellationToken cancellationToken);
+        public Task<List<FinalRegistrationDto>> Get(int ActivityId, CancellationToken cancellationToken);
+
+        public Task Delete(int FinalRegistrationId, int PersonDeletedId, CancellationToken cancellationToken);
+    }
+}
---
+++ Src/01-Domain/Services/App.Domain.AppServices/Activity/FinalRegistrationAppService.cs
@@ -0,0 +1,26 @@
+﻿using App.Domain.Core.Activity.AppServices;
+using App.Domain.Core.Activity.DTOs;
+using App.Domain.Core.Activity.Services;
+
+namespace App.Domain.AppServices.Activity
+{
+    public class FinalRegistrationAppService : IFinalRegistrationAppService
+    {
+        private readonly IFinalRegistrationService _finalRegistrationService;
+        public FinalRegistrationAppService(IFinalRegistrationService finalRegistrationService) => _finalRegistrationService = finalRegistrationService;
+        public async Task Add(int ActivityId, int PersoneId, int ResponsibilityId, int PersonCreatorId, CancellationToken cancellationToken)
+        {
+            await _finalRegistrationService.Add(ActivityId, PersoneId, ResponsibilityId, PersonCreatorId, cancellationToken);
+        }
+
+        public async Task Delete(int FinalRegistrationId, int PersonDeletedId, CancellationToken cancellationToken)
+        {
+            await _finalRegistrationService.Delete(FinalRegistrationId, PersonDeletedId, cancellationToken);
+        }
+
+        public async Task<List<FinalRegistrationDto>> Get(int ActivityId, CancellationToken cancellationToken)
+        {
+            return await _finalRegistrationService.Get(ActivityId, cancellationToken);
+        }
+    }
+}
