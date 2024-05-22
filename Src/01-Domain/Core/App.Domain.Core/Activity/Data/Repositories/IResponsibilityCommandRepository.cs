54f006ef6f65be713e2881bb8e23b047cde81889
Zoheir Allafzadeh <116369321+zoheirallafzadeh@users.noreply.github.com>
2024-05-22 15:36:25 +0330

    complite activity service and interface for repository

---
+++ Src/01-Domain/Core/App.Domain.Core/Activity/Data/Repositories/IResponsibilityCommandRepository.cs
@@ -0,0 +1,13 @@
+﻿using App.Domain.Core.Activity.DTOs;
+using App.Domain.Core.Application.Enum;
+
+namespace App.Domain.Core.Activity.Data.Repositories
+{
+    public interface IResponsibilityCommandRepository
+    {
+        public Task Add(string EnName, CancellationToken cancellationToken);
+        public Task Add(string EnName, int RelevantSkillsId, CancellationToken cancellationToken);
+        public Task Add(int ResponsibilityId, TranslateCode translateCode, string Name, CancellationToken cancellationToken);
+        public Task Update(ResponsibilityWithTranslationDto responsibilityWithTranslationDto, CancellationToken cancellationToken);
+    }
+}
---
+++ Src/01-Domain/Services/App.Domain.Services/App.Domain.Services/Person/PersonService.cs
@@ -0,0 +1,7 @@
+﻿
+namespace App.Domain.Services.Person
+{
+    internal class PersonService
+    {
+    }
+}
